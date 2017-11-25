4c5210867969bdf4018fc085d088dbcf633c7057
Rob - Laptop <ras12@albion.edu>
2017-11-24 22:28:55 -0600

    Reduces lag by relying on built in text to speech.  Eliminates need for pre-recorded mp3s.

--- app/src/main/java/com/MemoryLadder/SoundManager.java
+++ app/src/main/java/com/MemoryLadder/SoundManager.java
@@ -1,50 +1,28 @@
 package com.MemoryLadder;
 
-import java.util.HashMap;
 import android.content.Context;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.speech.tts.TextToSpeech;
 
-import com.mastersofmemory.memoryladder.R;
-//import com.MemoryLadderFull.R;
+import java.util.Locale;
 
-public class SoundManager {
- 
- private  SoundPool mSoundPool; 
- private  HashMap<Integer, Integer> mSoundPoolMap; 
- private  AudioManager  mAudioManager;
- private  Context mContext;
- 
- 
- public SoundManager(Context mContext) {
-   this.mContext = mContext;
-   initSounds();
- }
- 
- public void initSounds() {
-   mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0); 
-   mSoundPoolMap = new HashMap<Integer, Integer>(); 
-   mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);       
-   
-   addSound(0, R.raw.sound0);
-   addSound(1, R.raw.sound1);
-   addSound(2, R.raw.sound2);
-   addSound(3, R.raw.sound3);
-   addSound(4, R.raw.sound4);
-   addSound(5, R.raw.sound5);
-   addSound(6, R.raw.sound6);
-   addSound(7, R.raw.sound7);
-   addSound(8, R.raw.sound8);
-   addSound(9, R.raw.sound9);
- } 
- 
- public void addSound(int Index, int SoundID) {
-   mSoundPoolMap.put(Index, mSoundPool.load(mContext, SoundID, 2));
- }
- 
- public void playSound(int index) {
-   int streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC); 
-   mSoundPool.play(mSoundPoolMap.get(index), streamVolume, streamVolume, 1, 0, 1f); 
- }
- 
+class SoundManager {
+
+    private TextToSpeech speaker;
+
+    SoundManager(Context mContext) {
+        this.speaker = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    speaker.setLanguage(Locale.UK);
+                    speaker.setPitch(.8f);
+                }
+            }
+        });
+    }
+
+    void playSound(int index) {
+        speaker.speak(Integer.toString(index), TextToSpeech.QUEUE_FLUSH, null);
+        System.out.println("Play: " + index);
+    }
 }
