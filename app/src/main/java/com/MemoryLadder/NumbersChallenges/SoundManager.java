18c393be9c816f18f02bea6300ba5faed0fcd403
Rob - Laptop <ras12@albion.edu>
2018-02-25 12:41:45 -0500

    Adds legacy support if the text to speech functionality fails.

--- app/src/main/java/com/MemoryLadder/NumbersChallenges/SoundManager.java
+++ app/src/main/java/com/MemoryLadder/NumbersChallenges/SoundManager.java
@@ -1,31 +1,93 @@
 package com.MemoryLadder.NumbersChallenges;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.speech.tts.TextToSpeech;
+import android.util.SparseIntArray;
+
+import com.mastersofmemory.memoryladder.R;
 
 import java.util.Locale;
 
 class SoundManager {
 
+    private final Context mContext;
+    private final float speechRate;
     private TextToSpeech speaker;
 
+    /* Legacy fields only */
+    private boolean legacy = true;
+    private SoundPool mSoundPool;
+    private SparseIntArray mSoundPoolMap;
+    private AudioManager mAudioManager;
+
     SoundManager(Context mContext, float speechRate) {
+        this.mContext = mContext;
+        this.speechRate = speechRate;
         this.speaker = new TextToSpeech(mContext, status -> {
-            if(status != TextToSpeech.ERROR) {
-                speaker.setLanguage(Locale.UK);
-                speaker.setPitch(.8f);
-                speaker.setSpeechRate(speechRate);
+            if (status == TextToSpeech.SUCCESS) {
+                if (speaker != null) {
+                    int result = speaker.setLanguage(Locale.UK);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        setupLegacySpeaker();
+                    }
+                    else {
+                        speaker.setPitch(.8f);
+                        speaker.setSpeechRate(speechRate);
+                    }
+                }
+            }
+            else {
+                setupLegacySpeaker();
             }
         });
     }
 
     void playSound(int index) {
-        speaker.speak(Integer.toString(index), TextToSpeech.QUEUE_FLUSH, null);
-        System.out.println("Play: " + index);
+        if (legacy) {
+            playLegacySound(index);
+        }
+        else {
+            speaker.speak(Integer.toString(index), TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     public void stop() {
-        speaker.stop();
-        speaker.shutdown();
+        if (speaker != null) {
+            speaker.stop();
+            speaker.shutdown();
+        }
+    }
+
+    private void setupLegacySpeaker() {
+        initLegacySounds();
+        legacy = true;
+    }
+
+    private void initLegacySounds() {
+        mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        mSoundPoolMap = new SparseIntArray();
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
+        addLegacySound(0, R.raw.sound0);
+        addLegacySound(1, R.raw.sound1);
+        addLegacySound(2, R.raw.sound2);
+        addLegacySound(3, R.raw.sound3);
+        addLegacySound(4, R.raw.sound4);
+        addLegacySound(5, R.raw.sound5);
+        addLegacySound(6, R.raw.sound6);
+        addLegacySound(7, R.raw.sound7);
+        addLegacySound(8, R.raw.sound8);
+        addLegacySound(9, R.raw.sound9);
+    }
+
+    private void addLegacySound(int Index, int SoundID) {
+        mSoundPoolMap.put(Index, mSoundPool.load(mContext, SoundID, 2));
+    }
+
+    private void playLegacySound(int index) {
+        int streamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mSoundPool.play(mSoundPoolMap.get(index), streamVolume, streamVolume, 1, 0, speechRate);
     }
 }
